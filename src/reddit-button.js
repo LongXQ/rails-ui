ed53e16be73f824f485eea89a8508ce9a5b63ada
long <longxiongqiu@icloud.com>
2025-12-24 17:01:31 +0800

    init shoelace

---
+++ src/reddit-button.js
@@ -0,0 +1,130 @@
+import { LitElement, css, html } from 'lit'
+import litLogo from './assets/lit.svg'
+import viteLogo from '/vite.svg'
+
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
+// Set the base path to the folder you copied Shoelace's assets to
+setBasePath('../node_modules/@shoelace-style/shoelace/dist');
+
+// <sl-button>, <sl-icon>, <sl-input>, and <sl-rating> are ready to use!
+
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class RedditButton extends LitElement {
+  static get properties() {
+    return {
+      /**
+       * Copy for the read the docs hint.
+       */
+      docsHint: { type: String },
+
+      /**
+       * The number of times the button has been clicked.
+       */
+      count: { type: Number },
+    }
+  }
+
+  constructor() {
+    super()
+    this.docsHint = 'Click on the Vite and Lit logos to learn more'
+    this.count = 0
+  }
+
+  render() {
+    return html`
+      <sl-button>Button</sl-button>
+    `
+  }
+
+  _onClick() {
+    this.count++
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        max-width: 1280px;
+        margin: 0 auto;
+        padding: 2rem;
+        text-align: center;
+      }
+
+      .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+        transition: filter 300ms;
+      }
+      .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+      }
+      .logo.lit:hover {
+        filter: drop-shadow(0 0 2em #325cffaa);
+      }
+
+      .card {
+        padding: 2em;
+      }
+
+      .read-the-docs {
+        color: #888;
+      }
+
+      a {
+        font-weight: 500;
+        color: #646cff;
+        text-decoration: inherit;
+      }
+      a:hover {
+        color: #535bf2;
+      }
+
+      ::slotted(h1) {
+        font-size: 3.2em;
+        line-height: 1.1;
+      }
+
+      button {
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        background-color: #1a1a1a;
+        cursor: pointer;
+        transition: border-color 0.25s;
+      }
+      button:hover {
+        border-color: #646cff;
+      }
+      button:focus,
+      button:focus-visible {
+        outline: 4px auto -webkit-focus-ring-color;
+      }
+
+      @media (prefers-color-scheme: light) {
+        a:hover {
+          color: #747bff;
+        }
+        button {
+          background-color: #f9f9f9;
+        }
+      }
+    `
+  }
+}
+
+window.customElements.define('reddit-button', RedditButton)
